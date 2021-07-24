faf3e38f4168ba63bd4a4188ff743e7b1417432e
Raushan8271 <raushan8271@gmail.com>
2021-07-24 16:17:47 +0530

    done

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -5,7 +5,7 @@ import { AiOutlineMail } from "react-icons/ai";
 function Contact() {
     return (
         <div className="contact">
-            <h1 style={{ borderBottom: "2px solid black" }}>Contact me</h1>
+            <h1>GET IN TOUCH</h1>
             <div style={{ marginTop: "5%" }}>
                 <h1><FaMobileAlt /> +91-7856074204</h1>
                 <h1><AiOutlineMail /> raushan8271@gmail.com</h1>
--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import jiomart from '../files/jiomart.png'
+import fashionwala from '../files/fashionwala.png'
 
 function Project() {
     return (
         <div className="project">
             <h1>Project</h1>
+            <div className="projectdiv">
+                <div className="proFirstDiv">
+                    <div className="imgDiv">
+                        <img style={{ borderRadius: "10px" }} src={jiomart} alt="jiomart" />
+                        <div className="btnDiv">
+                            <a href="https://github.com/kamlendr/Jiomart" target="_blank" rel="noopener noreferrer"><button className="btnbtn">Source Code</button></a>
+                            <a href="https://jiomart.netlify.app/" rel="noreferrer" target="_blank"><button className="btnbtn">Demo</button></a>
+                        </div>
+                    </div>
+                    <div className="imgDiv">
+                        <img style={{ borderRadius: "10px" }} src={fashionwala} alt="jiomart" />
+                        <div className="btnDiv">
+                            <a href="https://fashionwala.netlify.app/" rel="noreferrer" target="_blank"><button className="btnbtn">Source Code</button></a>
+                            <a href="https://github.com/Raushan8271/fashionwala" rel="noreferrer" target="_blank"><button className="btnbtn">Demo</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
