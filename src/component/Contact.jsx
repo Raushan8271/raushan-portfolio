7e4efad86bd31ba38792a17fabb1b9bc5f65ee11
Raushan8271 <raushan8271@gmail.com>
2021-08-01 22:57:50 +0530

    assignment

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -5,10 +5,20 @@ import { AiOutlineMail } from "react-icons/ai";
 function Contact() {
     return (
         <div className="contact">
-            <h1>GET IN TOUCH</h1>
-            <div style={{ marginTop: "5%" }}>
-                <h1><FaMobileAlt /> +91-7856074204</h1>
-                <h1><AiOutlineMail /> raushan8271@gmail.com</h1>
+            <h1 className="getin">GET IN TOUCH</h1>
+            <div className="contactDiv">
+                <div>
+                    <div className="mobile">
+                        <h1 style={{ margin: "0", color: "#fff" }}>Mobile No:</h1>
+                        <a style={{ color: "#fff" }} className="noa" href="tel:+91-7856074204"><FaMobileAlt /> +91-7856074204</a>
+                    </div>
+                </div>
+                <div>
+                    <div className="mobile">
+                        <h1 style={{ margin: "0", color: "#fff" }}>Email:</h1>
+                        <a style={{ color: "#fff" }} className="noa" href="mailto:raushan8271@gmail.com"><AiOutlineMail />raushan8271@gmail.com</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
